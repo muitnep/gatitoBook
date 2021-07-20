be2d8be7a04ad5ed34cacbcf74ff7bf854115fdb
Rodrigo Santos Bonifácio Rodrigues <muitnep@gmail.com>
2021-07-20 09:06:29 -0300

    projeto curso angular da alura

---
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -0,0 +1,23 @@
+import { Router } from '@angular/router';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.css']
+})
+export class CabecalhoComponent {
+
+  user$ = this.usuarioService.retornaUsuario();
+
+
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
+
+  logOut() {
+    this.usuarioService.logOut();
+    this.router.navigate(['']);
+  }
+
+
+}
